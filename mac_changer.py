436e68213e65d1f1bb1e1feab02fcf414975e685
arronh12 <arronhoare@gmail.com>
2020-05-08 21:00:10 -0400

    python script that changes MAC of linux device - added parser features.

--- mac_changer.py
+++ mac_changer.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
 from pip._vendor.distlib.compat import raw_input
+
+prs = optparse.OptionParser()
+prs.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address...")
+prs.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+prs.parse_args()
 
 interface = raw_input("Interface > ")
 new_mac = raw_input("New MAC > ")
 
 print("[+] changing MAC address for " + interface + "to " + new_mac)
 
-subprocess.call('ifconfig ' + interface + ' down', shell=True)
-subprocess.call('ifconfig ' + interface + ' hw ether '+ new_mac, shell=True)
-subprocess.call('ifconfig ' + interface + ' up', shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
+
