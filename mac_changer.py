0feeb9ca811de154c5a5d96650481706ead74047
arronh12 <arronhoare@gmail.com>
2020-05-11 11:34:19 -0400

    python script that changes MAC of linux device - organised code into functions, added user input for interface and new MAC.

--- mac_changer.py
+++ mac_changer.py
@@ -3,20 +3,21 @@
 import subprocess
 import optparse
 
-from pip._vendor.distlib.compat import raw_input
+def get_arguments():
+    prs = optparse.OptionParser()
+    prs.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address...")
+    prs.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    return prs.parse_args()
 
-prs = optparse.OptionParser()
-prs.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address...")
-prs.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
-prs.parse_args()
 
-interface = raw_input("Interface > ")
-new_mac = raw_input("New MAC > ")
+def change_mac(interface, new_mac):
+    print("[+] changing MAC address for " + interface + "to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-print("[+] changing MAC address for " + interface + "to " + new_mac)
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
 
