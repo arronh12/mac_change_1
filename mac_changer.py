58b23ca3aaa27e88e2ca4d1cf443b1cbf295335a
arronh12 <arronhoare@gmail.com>
2020-05-13 22:06:33 -0400

    python script that changes MAC of linux device - cleaned up code, added function to check user input has successfully changed MAC address, added printed out user responses.

--- mac_changer.py
+++ mac_changer.py
@@ -4,11 +4,14 @@ import subprocess
 import optparse
 import re
 
+
+# function to define and add parse options.
 def get_arguments():
     prs = optparse.OptionParser()
     prs.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address...")
     prs.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
     (options, arguments) = prs.parse_args()
+
     if not options.interface:
         prs.error("[*] Error. Please specify an interface, use --help for more info.")
     elif not options.new_mac:
@@ -16,19 +19,39 @@ def get_arguments():
     return options
 
 
+# function to change MAC address for specific interface
 def change_mac(interface, new_mac):
-    print("[+] changing MAC address for " + interface + "to " + new_mac)
+    print("[+] changing MAC address for " + interface + " to " + new_mac)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-options = get_arguments()
-change_mac(options.interface, options.new_mac)
-result = subprocess.check_output(["ifconfig", options.interface])
-print(result)
-captured_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", result)
 
-if captured_mac:
-    print("[*] New MAC address: " + captured_mac.group(0))
-else:
-    print("[*] Error - could not read MAC address.")
+# function to use regex module that extracts MAC address from ifconfig command
+def current_mac(interface):
+    result = subprocess.check_output(["ifconfig", interface])
+    captured_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", result)
+
+    if captured_mac:
+        return captured_mac.group(0)
+    else:
+        print("[*] Error - could not read MAC address.")
+
+
+#function to validate that MAC has been changed to user input
+def validate_mac(new_mac):
+    if new_mac == options.new_mac:
+        print("[*] MAC address successfully changed too " + str(new_mac))
+    else:
+        print("[*] Error - MAC address did not change, use --help for more information")
+
+
+# function calls
+options = get_arguments()
+mac = current_mac(options.interface)
+print("[*] Current MAC: " + str(mac))
+change_mac(options.interface, options.new_mac)
+validate_mac(current_mac(options.interface))
+
+
+
